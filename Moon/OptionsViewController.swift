c4541cdef730b7d2702b393247c063dc463981a4
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2016-01-25 23:48:39 +0530

    Added preferences view

---
+++ Moon/OptionsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OptionsViewController.swift
+//  Moon
+//
+//  Created by Kaunteya Suryawanshi on 25/01/16.
+//  Copyright © 2016 Kaunteya Suryawanshi. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UITableViewController {
+
+    @IBAction func donePressed(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
